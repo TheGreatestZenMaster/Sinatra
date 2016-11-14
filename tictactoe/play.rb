644a904c794f90021212598e82c963623e7e968a
TheGreatestZenMaster <jetloose@gmail.com>
2016-11-14 05:55:10 +0000

    Refactored tictactoe

--- tictactoe/play.rb
+++ tictactoe/play.rb
@@ -7,61 +7,31 @@ class TicTacToe<Sinatra::Base
 	@@spaces = [:one, :two, :three, :four, :five, :six, :seven, :eight, :nine].to_a
 	
     get '/' do
-    	message = "When you're ready!"
-    	board = @@game.display.board
+    	@message = "When you're ready!"
+    	@board = @@game.display.board
     	@spaces = @@spaces
-        erb :index, :locals => { :board => board, :message => message }
+        erb :index
     end
     post '/tictactoe' do
     	@spaces = @@spaces
     	@@count += 1
     	@count = @@count % 2
-    	board = @@game.display.board
-    	puts params.inspect
-    	@move = params.keys[0]
+    	@board = @@game.display.board
     	if params['reset']
-    		message = @@game.reset_game
-    		board = @@game.display.board
-    		erb :index, :locals => { :board => board, :message => message }
+    		@message = @@game.reset_game
+    		@board = @@game.display.board
+    		erb :index
     	else
-	    	if valid(params[@move])
-	    		update(params[@move], @move.to_sym)
-	    		message = "Nice Move!"
+    		@move = params.keys[0]
+	    	if @@game.valid(params[@move])
+	    		@@game.display.update(params[@move], @move.to_sym)
+	    		@message = "Nice Move!"
 			else
-				message = "That's not a valid input!"
+				@message = "That's not a valid input!"
 			end
-			message = "Tie!" if check_for_tie
-			message = "Yeah! You won!" if check_for_victory(params[@move])
-			erb :index, :locals => { :board => board, :message => message}
+			@message = "Tie!" if @@game.check_for_tie
+			@message = "Yeah! You won!" if @@game.check_for_victory(params[@move])
+			erb :index
 		end
     end
-
-    def update(symbol, location)
-		@@game.display.board[location] = symbol
-	end
-	
-	def valid(num)
-		valid = false
-		if num == 'X' || num == 'O'
-			valid = true
-		end
-		valid
-	end
-	
-	def check_for_victory(symbol)
-		victory = false
-		victory_conditions = [[:one, :two, :three], [:four, :five, :six], [:seven, :eight, :nine], [:one, :four, :seven],[:two, :five, :eight],[:three, :six, :nine],[:one, :five, :nine],[:seven, :five, :three]].to_a
-		victory_conditions.each do |sub_array|
-			if @@game.display.board[sub_array[0]] == symbol && @@game.display.board[sub_array [1]] == symbol && @@game.display.board[sub_array[2]] == symbol
-				victory = true
-			end
-		end
-		return victory
-	end
-	
-	def check_for_tie
-		tie = false
-		tie = true if @@game.display.board.all?{|x,y| y.is_a?(String)}
-		tie
-	end
 end
--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -4,23 +4,32 @@ class Game
 	attr_accessor :display
 	#initializes the game and then starts one
 	def initialize()
-		#puts "Greetings Challengers! Time to test yourselves!"
-		#puts "Player 1: What is your name?"
-		#@player1 = Player.new("Jake")
-		#puts "Player 2: What is your name?"
-		#@player2 = Player.new("Bob")
 		@display = Display.new	
 	end
 
-	#asks the player if they want to continue
-	def new_game?
-		_response = ""
-		until _response == "yes" || _response == "no"
-			puts "Would you like to continue? Yes/No"
-			_response = gets.chomp.downcase
-			_response == "yes"? new_game = true : new_game = false
+	def valid(num)
+		valid = false
+		if num == 'X' || num == 'O'
+			valid = true
 		end
-		new_game
+		valid
+	end
+	
+	def check_for_tie
+		tie = false
+		tie = true if @display.board.all?{|x,y| y.is_a?(String)}
+		tie
+	end
+	
+	def check_for_victory(symbol)
+		victory = false
+		victory_conditions = [[:one, :two, :three], [:four, :five, :six], [:seven, :eight, :nine], [:one, :four, :seven],[:two, :five, :eight],[:three, :six, :nine],[:one, :five, :nine],[:seven, :five, :three]].to_a
+		victory_conditions.each do |sub_array|
+			if @display.board[sub_array[0]] == symbol && @display.board[sub_array [1]] == symbol && @display.board[sub_array[2]] == symbol
+				victory = true
+			end
+		end
+		return victory
 	end
 	
 	#resets the game
@@ -29,162 +38,22 @@ class Game
 		return "The Game has been reset!"
 	end
 end
-=begin
-class Player
-	attr_accessor :symbol, :name, :wins, :loses, :turn
-	@@symbols = ["X", "O"].to_a
-	@@turns = [:first, :second].to_a
-	
-	#initalizes the players
-	def initialize(name)
-		@name = name
-		@symbol = ""
-		choose_symbol 
-		@turn = ""
-		choose_turn
-		@wins = 0
-		@loses = 0
-		puts "#{@name} is '#{@symbol}'. #{@name} goes #{@turn}."
-	end
-	
-	#checks for victory condition
-	def check_for_victory(display)
-		victory = false
-		victory_conditions = [[:one, :two, :three], [:four, :five, :six], [:seven, :eight, :nine], [:one, :four, :seven],[:two, :five, :eight],[:three, :six, :nine],[:one, :five, :nine],[:seven, :five, :three]].to_a
-		victory_conditions.each do |sub_array|
-			if display[sub_array[0]] == @symbol && display[sub_array [1]] == @symbol && display[sub_array[2]] == @symbol
-				victory = true
-				@wins += 1
-				@turn = :second
-			end
-		end
-		return victory
-	end
-	
-	#chooses the players' symbols randomly
-	def choose_symbol
-		if @@symbols.count == 2
-			@symbol = @@symbols[rand(0..1)]
-			@@symbols.delete(@symbol)
-		else
-			@symbol = @@symbols[0]
-			@@symbols.delete_at(0)
-		end
-	end
-	
-	#chooses who goes first randomly
-	def choose_turn
-		if @@turns.count == 2
-			@turn = @@turns[rand(0..1)]
-			@@turns.delete(@turn)
-		else
-			@turn = @@turns[0]
-			@@turns.delete_at(0)
-		end
-	end
 
-	#shows the stats
-	def display_stats
-		print "#{@name} has #{@wins} wins!"
-	end
-	
-	#processes the move of the player
-	def make_move(display)
-		puts "It is your turn #{@name}!"
-		display.update(self, display.choose)
-	end
-end
-=end
-	
 class Display
 	attr_accessor :board
-	#initializes the game board hash
+
 	def initialize
-		@linewidth = 45
 		@board = Hash[:one => 1, :two => 2, :three => 3, :four => 4, :five => 5, :six => 6, :seven => 7, :eight => 8, :nine => 9]
-		#puts ""
-		#puts "Let's begin".center(@linewidth)
-		#puts self.display
-	end
-	
-	#Simple display method that shows the board
-	def display
-		breakline = "---- --- ----"
-		puts breakline.center(@linewidth)
-		puts "| #{@board[:one]} | #{@board[:two]} | #{@board[:three]} |".center(@linewidth)
-		puts breakline.center(@linewidth)
-		puts "| #{@board[:four]} | #{@board[:five]} | #{@board[:six]} |".center(@linewidth)
-		puts breakline.center(@linewidth)
-		puts "| #{@board[:seven]} | #{@board[:eight]} | #{@board[:nine]} |".center(@linewidth)
-		puts breakline.center(@linewidth)
-		puts "".center(@linewidth)
 	end
 	
 	#updates the hash that stores the values for the board
 	def update(player, location)
-		puts "#{player.name} made their move!"
-		@board[location.to_sym] = player.symbol
-		self.display
+		@board[location] = player
 	end
 	
 	#resets the board to its original state after a game
 	def reset
 		@board = Hash[:one => 1, :two => 2, :three => 3, :four => 4, :five => 5, :six => 6, :seven => 7, :eight => 8, :nine => 9]
-	end
-	
-	#gets input and makes sure it is valid
-	def choose
-		print "Please choose a number!"
-		num = gets.chomp
-		valid = false
-		until valid
-			if num.match(/\d+/)
-				num = convert_to_word(num.to_i).to_sym
-			else
-				num = num.downcase.to_sym
-			end
-			available = (@board[num] != "X" && @board[num] != "O")
-			if (@board.keys.include? num) && available
-				valid = true
-			else
-				puts "Sorry that is an illegal move!"
-				print "Please choose a number! "
-				num = gets.chomp
-			end
-		end
-		num
-	end
-	
-	def convert_to_num(num)
-		num.downcase!
-		case num
-		when "one" then 1
-		when "two" then 2
-		when "three" then 3
-		when "four" then 4
-		when "five" then 5
-		when "six" then 6
-		when "seven" then 7
-		when "eight" then 8
-		when "nine" then 9
-		end
-	end
-	
-	def convert_to_word(num)
-		case num
-		when 1 then x = "one"
-		when 2 then x = "two"
-		when 3 then x ="three"
-		when 4 then x ="four"
-		when 5 then x ="five"
-		when 6 then x ="six"
-		when 7 then x = "seven"
-		when 8 then x = "eight"
-		when 9 then x ="nine"
-		else
-			x = "zero"
-		end
-		x
 	end
 end
 
